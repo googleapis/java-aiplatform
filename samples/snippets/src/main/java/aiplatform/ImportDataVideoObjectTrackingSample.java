ec28c53eaf9885e42fd821b4f7a22be54c0001c6
Mike Ganbold <munkhbayarg@google.com>
2020-09-30 15:12:20 -0700

    fixed the lint issue

--- samples/snippets/src/main/java/aiplatform/ImportDataVideoObjectTrackingSample.java
+++ samples/snippets/src/main/java/aiplatform/ImportDataVideoObjectTrackingSample.java
@@ -78,7 +78,8 @@ public class ImportDataVideoObjectTrackingSample {
       System.out.println("Waiting for operation to finish...");
       ImportDataResponse importDataResponse = importDataResponseFuture.get(300, TimeUnit.SECONDS);
 
-      System.out.format("Import Data Video Object Tracking Response: %s\n", importDataResponse.toString());
+      System.out.format("Import Data Video Object Tracking Response: %s\n",
+              importDataResponse.toString());
     }
   }
 }

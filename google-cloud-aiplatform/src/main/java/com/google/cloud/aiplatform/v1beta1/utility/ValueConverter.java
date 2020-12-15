393458db3d74ae19a9945ec7a22b00384d4265eb
Eric Schmidt <erschmid@google.com>
2020-12-14 21:24:18 -0800

    feat: adds ValueConverter tests

--- google-cloud-aiplatform/src/main/java/com/google/cloud/aiplatform/v1beta1/utility/ValueConverter.java
+++ google-cloud-aiplatform/src/main/java/com/google/cloud/aiplatform/v1beta1/utility/ValueConverter.java
@@ -6,22 +6,20 @@ import com.google.protobuf.Value;
 import com.google.protobuf.util.JsonFormat;
 
 /**
- * Exposes utility methods for converting AI Platform messages to and
- * from {@com.google.protobuf.Value} objects.
+ * Exposes utility methods for converting AI Platform messages to and from
+ * {@com.google.protobuf.Value} objects.
  */
 public class ValueConverter {
 
-  /**
-   * An empty {@com.google.protobuf.Value} message.
-   */
+  /** An empty {@com.google.protobuf.Value} message. */
   public static final Value EMPTY_VALUE = Value.newBuilder().build();
 
   /**
    * Converts a message type to a {@com.google.protobuf.Value}.
    *
-   * @param message   the message to convert
-   * @return          the message as a {@com.google.protobuf.Value}
-   * @throws          InvalidProtocolBufferException
+   * @param message the message to convert
+   * @return the message as a {@com.google.protobuf.Value}
+   * @throws InvalidProtocolBufferException
    */
   public static Value toValue(Message message) throws InvalidProtocolBufferException {
     String jsonString = JsonFormat.printer().print(message);
@@ -31,13 +29,13 @@ public class ValueConverter {
   }
 
   /**
-   * Converts a {@com.google.protobuf.Value} to a {@com.google.protobuf.Message}
-   * of the provided {@com.google.protobuf.Message.Builder}.
+   * Converts a {@com.google.protobuf.Value} to a {@com.google.protobuf.Message} of the provided
+   * {@com.google.protobuf.Message.Builder}.
    *
-   * @param messageBuilder  a builder for the message type
-   * @param value           the Value to convert to a message
-   * @return                the value as a message
-   * @throws                InvalidProtocolBufferException
+   * @param messageBuilder a builder for the message type
+   * @param value the Value to convert to a message
+   * @return the value as a message
+   * @throws InvalidProtocolBufferException
    */
   public static Message fromValue(Message.Builder messageBuilder, Value value)
       throws InvalidProtocolBufferException {
---
+++ google-cloud-aiplatform/src/test/java/com/google/cloud/aiplatform/v1beta1/utility/ValueConverterTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.aiplatform.v1beta1.utility;
+
+import com.google.cloud.aiplatform.v1beta1.schema.trainingjob.definition.AutoMlImageClassificationInputs;
+import com.google.cloud.aiplatform.v1beta1.schema.trainingjob.definition.AutoMlImageClassificationInputs.ModelType;
+import com.google.gson.JsonObject;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.MapEntry;
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
+import com.google.protobuf.util.JsonFormat;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ValueConverterTest {
+
+  private static JsonObject testJsonInputs;
+  private static Value testValueInputs;
+  private static AutoMlImageClassificationInputs testObjectInputs;
+
+  @Before
+  public void setUp() throws InvalidProtocolBufferException {
+    testJsonInputs = new JsonObject();
+    testJsonInputs.addProperty("multi_label", true);
+    testJsonInputs.addProperty("model_type", "CLOUD");
+    testJsonInputs.addProperty("budget_milli_node_hours", 8000);
+
+    Value.Builder valueBuilder = Value.newBuilder();
+    JsonFormat.parser().merge(testJsonInputs.toString(), valueBuilder);
+    testValueInputs = valueBuilder.build();
+
+    testObjectInputs =
+        AutoMlImageClassificationInputs.newBuilder()
+            .setModelType(ModelType.CLOUD)
+            .setBudgetMilliNodeHours(8000)
+            .setMultiLabel(true)
+            .setDisableEarlyStopping(false)
+            .build();
+  }
+
+  @Test
+  public void testValueConverterToValue() throws InvalidProtocolBufferException {
+    Value actualConvertedValue = ValueConverter.toValue(testObjectInputs);
+
+    Struct actualStruct = actualConvertedValue.getStructValue();
+    Assert.assertEquals(3, actualStruct.getFieldsCount());
+
+    Collection<Object> innerFields = actualStruct.getAllFields().values();
+    Collection<MapEntry> fieldEntries = (Collection<MapEntry>) innerFields.toArray()[0];
+
+    MapEntry actualBoolValueEntry =
+        fieldEntries.stream()
+            .filter(item -> (item.getKey().toString().contains("multiLabel")))
+            .collect(Collectors.toList())
+            .get(0);
+    Value actualBoolValue = (Value) actualBoolValueEntry.getValue();
+    Assert.assertEquals(testObjectInputs.getMultiLabel(), actualBoolValue.getBoolValue());
+
+    MapEntry actualStringValueEntry =
+        fieldEntries.stream()
+            .filter(item -> (item.getKey().toString().contains("modelType")))
+            .collect(Collectors.toList())
+            .get(0);
+    Value actualStringValue = (Value) actualStringValueEntry.getValue();
+    Assert.assertEquals("CLOUD", actualStringValue.getStringValue());
+
+    MapEntry actualNumberValueEntry =
+        fieldEntries.stream()
+            .filter(item -> (item.getKey().toString().contains("budgetMilliNodeHours")))
+            .collect(Collectors.toList())
+            .get(0);
+    Value actualNumberValue = (Value) actualNumberValueEntry.getValue();
+    // protobuf stores int64 values as strings rather than numbers
+    long actualNumber = Long.parseLong(actualNumberValue.getStringValue());
+    Assert.assertEquals(testObjectInputs.getBudgetMilliNodeHours(), actualNumber);
+  }
+
+  @Test
+  public void testValueConverterFromValue() throws InvalidProtocolBufferException {
+    AutoMlImageClassificationInputs actualInputs =
+        (AutoMlImageClassificationInputs)
+            ValueConverter.fromValue(AutoMlImageClassificationInputs.newBuilder(), testValueInputs);
+
+    Assert.assertEquals(8000, actualInputs.getBudgetMilliNodeHours());
+    Assert.assertEquals(true, actualInputs.getMultiLabel());
+    Assert.assertEquals(ModelType.CLOUD, actualInputs.getModelType());
+  }
+}

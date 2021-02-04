a6f51de9f487eec31902d5c917285287ed0f7514
yoshi-automation <yoshi-automation@google.com>
2021-02-04 15:45:47 -0800

    chore: update gapic-generator-java to 0.0.18
    Committer: @miraleung
    PiperOrigin-RevId: 350949863
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Sat Jan 9 14:01:49 2021 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 91e206bcfeaf8948ea03fe3cb1b7616108496cd3
    Source-Link: https://github.com/googleapis/googleapis/commit/91e206bcfeaf8948ea03fe3cb1b7616108496cd3

--- google-cloud-aiplatform/src/main/java/com/google/cloud/aiplatform/v1beta1/DatasetServiceClient.java
+++ google-cloud-aiplatform/src/main/java/com/google/cloud/aiplatform/v1beta1/DatasetServiceClient.java
@@ -92,7 +92,7 @@ import javax.annotation.Generated;
  * <p>Please refer to the GitHub repository's samples for more quickstart code snippets.
  */
 @BetaApi
-@Generated("by gapic-generator")
+@Generated("by gapic-generator-java")
 public class DatasetServiceClient implements BackgroundResource {
   private final DatasetServiceSettings settings;
   private final DatasetServiceStub stub;
--- google-cloud-aiplatform/src/main/java/com/google/cloud/aiplatform/v1beta1/EndpointServiceClient.java
+++ google-cloud-aiplatform/src/main/java/com/google/cloud/aiplatform/v1beta1/EndpointServiceClient.java
@@ -96,7 +96,7 @@ import javax.annotation.Generated;
  * <p>Please refer to the GitHub repository's samples for more quickstart code snippets.
  */
 @BetaApi
-@Generated("by gapic-generator")
+@Generated("by gapic-generator-java")
 public class EndpointServiceClient implements BackgroundResource {
   private final EndpointServiceSettings settings;
   private final EndpointServiceStub stub;
